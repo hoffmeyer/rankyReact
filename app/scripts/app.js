13f63eafcf9f712955bfbda811ba590cc4356bcb
Flemming Hoffmeyer <flemming@festina-lente.dk>
2015-04-03 10:55:52 +0200

    keys on players

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -13,7 +13,7 @@ feed.playersUpdated(function(data) {
 var RankList = React.createClass({
   render: function() {
     var createItem = function(player) {
-      return <tr><td>{player.id}</td><td>{player.name}</td><td>{player.points}</td></tr>;
+      return <tr key={player.id}><td>{player.id}</td><td>{player.name}</td><td>{player.points}</td></tr>;
     };
     return <table>{this.props.players.map(createItem)}</table>;
   }
