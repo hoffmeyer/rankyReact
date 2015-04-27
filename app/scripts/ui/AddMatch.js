f27c83bc64b2a6438c89ae0c654beac6aab12d99
Flemming Hoffmeyer <flemming@festina-lente.dk>
2015-04-27 09:05:27 +0200

    Form for register match created

--- app/scripts/ui/AddMatch.js
+++ app/scripts/ui/AddMatch.js
@@ -4,7 +4,51 @@ var React = require('react');
 
 var AddMatch = React.createClass({
     render: function(){
-        return <h1>AddMatch</h1>;
+        return <form className="form-horizontal" >
+                <h3>Team1</h3>
+                <div className="form-group">
+                    <label className="col-sm-2 control-label" for="team1player1">Player 1</label>
+                    <div className="col-sm-10">
+                        <input className="form-control" type="text" id="team1player1" autoFocus="true" />
+                    </div>
+                </div>
+                <div className="form-group">
+                    <label className="col-sm-2 control-label" for="team1player2">Player 2</label>
+                    <div className="col-sm-10">
+                        <input className="form-control" type="text" id="team1player2"/>
+                    </div>
+                </div>
+                <div className="form-group">
+                    <label className="col-sm-2 control-label" for="team1score">Score</label>
+                    <div className="col-sm-2">
+                        <input className="form-control" type="text" id="team1score"/>
+                    </div>
+                </div>
+
+                <h3>Team2</h3>
+                <div className="form-group">
+                    <label className="col-sm-2 control-label" for="team2player1">Player 1</label>
+                    <div className="col-sm-10">
+                        <input className="form-control" type="text" id="team2player1" />
+                    </div>
+                </div>
+                <div className="form-group">
+                    <label className="col-sm-2 control-label" for="team2player2">Player 2</label>
+                    <div className="col-sm-10">
+                        <input className="form-control" type="text" id="team2player2"/>
+                    </div>
+                </div>
+                <div className="form-group">
+                    <label className="col-sm-2 control-label" for="team2score">Score</label>
+                    <div className="col-sm-2">
+                        <input className="form-control" type="text" id="team2score"/>
+                    </div>
+                </div>
+                <div className="form-group">
+                    <button className="btn btn-primary pull-right" >Register</button>
+                </div>
+            </form>
+
     }
 });
 
